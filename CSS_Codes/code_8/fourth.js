97ad983222c42ef67bff6fc5a80539d08d233906
Arzaan <25bee110@nith.ac.in>
2026-07-14 20:51:27 +0530

    Username Generator

---
+++ CSS_Codes/code_8/fourth.js
@@ -0,0 +1,10 @@
+
+let firstName = prompt("Enter your first name : ");
+let lastName = prompt("Enter your last name : ");
+
+let str1 = firstName + lastName ;
+let len = str1.length;
+
+const output = `@${str1}${len}`
+
+console.log("The username is : " ,output )
