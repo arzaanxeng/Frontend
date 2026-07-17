573a3f6bac96d034582c49ab1db0df086fccbc12
Arzaan <25bee110@nith.ac.in>
2026-07-18 02:17:24 +0530

    Events fundamental Understanding

--- CSS_Codes/code_17/Events.js
+++ CSS_Codes/code_17/Events.js
@@ -10,8 +10,10 @@ el.addEventListener("click" , ()=>{
 const element = document.getElementById("parent");
 console.dir(element.children); // We have three children
 
+// We can see how optimally we are able to write our code using for each child!
 for(let child of element.children){
     child.addEventListener("click" , ()=>{
     child.textContent = "Well , u can definitely see the text has changed!";
 })
-}
+} 
+
